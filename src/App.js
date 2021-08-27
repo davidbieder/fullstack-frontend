ab8f7c3c3d9af3769af2b3a69618867b564a6254
David Bieder <david.bieder@upslope.io>
2021-08-27 23:12:24 +0200

    build: optimize dockerization time and image size

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>
-          Version: 2
-        </p>
-      </header>
-    </div>
-  );
+const getUsers = async () => {
+  const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/users`)
+  return await response.json()
 }
 
-export default App;
+const App = () => {
+  const [ users, setUsers ] = useState([])
+
+  useEffect(() => {
+    getUsers().then(setUsers)
+  }, [])
+
+  return (
+    <>
+      <h1>Frontend</h1>
+      <ul>{users.map(user => <li key={user.name}>{user.name}</li>)}</ul>
+      <p>Version: 2</p>
+    </>
+  )
+}
+
+export default App
